2d7fe9194eccd2613cc2d8bf28a3ab6a5ead993f
potykion <potykion@gmail.com>
2024-11-29 22:25:59 +0300

    group by

--- beer-explorer-vue/app/pages/index.vue
+++ beer-explorer-vue/app/pages/index.vue
@@ -15,8 +15,25 @@ const groupers = [
   }
 ]
 
+const selectedGroups = ref([])
 
+// Функция для группировки пива
+const groupedBeers = computed(() => {
+  if (!beers.value || selectedGroups.value.length === 0) {
+    return { ungrouped: beers.value || [] }
+  }
 
+  const groupBy = selectedGroups.value[0] // Берем первую выбранную группировку
+  
+  return beers.value.reduce((acc, beer) => {
+    const key = beer[groupBy]
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(beer)
+    return acc
+  }, {})
+})
 </script>
 
 <template>
@@ -26,10 +43,10 @@ const groupers = [
     <v-row class="pt-5">
       <v-col>
         <v-select
+            v-model="selectedGroups"
             chips
             label="Группировка"
             :items="groupers"
-
             multiple
             variant="outlined"
             density="compact"
@@ -44,24 +61,28 @@ const groupers = [
             variant="outlined"
             density="compact"
         ></v-select>
-
       </v-col>
     </v-row>
 
-
-    <div >
-      <v-list density="compact">
-        <v-list-item
-            v-for="beer in beers"
-            :key="`${beer.brewery}-${beer.name}`"
-            :href="beer.url"
-            target="_blank"
-        >
-          <v-list-item-title>
-            {{ beer.brewery }} — {{ beer.name }} ({{ beer.rating }})
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
+    <div>
+      <template v-for="(beersInGroup, groupName) in groupedBeers" :key="groupName">
+        <v-list-subheader v-if="selectedGroups.length > 0">
+          {{ groupName }}
+        </v-list-subheader>
+        
+        <v-list density="compact">
+          <v-list-item
+              v-for="beer in beersInGroup"
+              :key="`${beer.brewery}-${beer.name}`"
+              :href="beer.url"
+              target="_blank"
+          >
+            <v-list-item-title>
+              {{ beer.brewery }} — {{ beer.name }} ({{ beer.rating }})
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </template>
     </div>
   </v-container>
 </template>
