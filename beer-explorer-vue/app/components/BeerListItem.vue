75e166fe9b710bf857dc1a1a642bbafd32fab569
potykion <potykion@gmail.com>
2025-02-21 16:41:33 +0300

    beer list item refactor

--- beer-explorer-vue/app/components/BeerListItem.vue
+++ beer-explorer-vue/app/components/BeerListItem.vue
@@ -7,26 +7,27 @@ interface Beer {
   style: string
 }
 
-
 interface Props {
   beer: Beer;
 }
 
 const props = defineProps<Props>()
-
 </script>
 
 <template>
   <v-list-item
-
       :href="beer.url"
-      :title='`<span class="font-weight-bold text-body-1">${beer.name}</span> <span class="text-body-2 font-italic">${beer.style}</span>`'
       target="_blank"
-      :subtitle='`${props.beer.brewery} • ${beer.rating} ⭐`'
   >
+    <template v-slot:title>
+      <div>
+        <span class="font-weight-bold text-body-1">{{ beer.name }}</span>&nbsp;
+        <span class="text-body-2 font-italic">{{ beer.style }}</span>
+      </div>
+    </template>
 
-    <template v-slot:title="{ title }">
-      <div v-html="title"></div>
+    <template v-slot:subtitle>
+      {{ props.beer.brewery }} • {{ beer.rating }} ⭐
     </template>
   </v-list-item>
-</template> 
+</template>
